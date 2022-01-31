690cb94d84fa6ca05ee577eb09288b01dcfba978
tamardayan2008 <87135031+tamardayan2008@users.noreply.github.com>
2022-01-31 17:32:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.ev3devices import (Motor, ColorSensor,  GyroSensor)
 from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
@@ -19,26 +18,53 @@ ev3 = EV3Brick()
 # Write your program here.
 ev3.speaker.beep()
 
-left_motor= Motor(Port.C)
-right_motor= Motor(Port.D)
-tiny_motor= Motor(Port.B)
-gyro= GyroSensor(Port.S4)
+left_motor= Motor(Port.C) #מנוע ימני
+right_motor= Motor(Port.B) #מנוע שמאלי
+tiny_motor= Motor(Port.D) #מנוע בינוני
+gyro= GyroSensor(Port.S4) #ג'יירו
 
-robot = DriveBase(left_motor, right_motor, 56.0, 100.0)
-robot.settings(900, 900,  85, 85)
+robot= DriveBase(left_motor, right_motor, wheel_diameter=56, axle_track=100) #הגדרות נסיעה ישר 
+robot.settings(900, 900,  72, 72) #הגדרות מהירות נסיעה
 
-robot.straight(261) #נוסעת ישר עם המשאית
-robot.turn(40) #פונה
-robot.straight(252) #מביאה את שתי המשאיות לגשר
-tiny_motor.run_angle(200, 35, then=Stop.BRAKE, wait=True) #יוצאת מהמשאית
+robot.straight(226) #נוסעת ישר עם המשאית
+robot.turn(48) #פונה
+robot.straight(320) #מביאה את שתי המשאיות לגשר
+tiny_motor.run_angle(100, -90, then=Stop.BRAKE, wait=True) #יוצאת מהמשאית
 tiny_motor.stop() #הפסקה
-robot.straight(250) #הולכת לגשר הקרוב
-tiny_motor.run_angle(-200, -215, then=Stop.BRAKE, wait=True) #מורידה את הגשר הקרוב
+robot.turn(2) #פונה
+robot.straight(261) #הולכת לגשר הקרוב
+tiny_motor.run_angle(-200, -230, then=Stop.BRAKE, wait=True) #מורידה את הגשר הקרוב
 tiny_motor.stop() #הפסקה
+robot.turn(1.5) #פונה
 robot.straight(200) #נוסעת לגשר הרחוק
-robot.turn(1) #פונה
 tiny_motor.run_angle(-200, 185, then=Stop.BRAKE, wait=True) #מורידה את הגשר הרחוק
 tiny_motor.stop() #הפסקה
 
-robot.straight(70) #מתקדמת לעיגול של קרגו קונקט
-tiny_motor.run_angle(200, 160, then=Stop.BRAKE, wait=True) #מזיזה את היד כדי שתהיה מוכנה לרכבת
+
+robot.straight(100) #מתקדמת לעיגול של קרגו קונקט
+robot.turn(-60) #פונה
+robot.straight(100) #מניחה את המודל חדשנות בתוך העיגול של קרגו קונקט
+robot.straight(-55) #יוצאת מהעיגול של קרגו קונקט
+robot.turn(40) #פונה
+tiny_motor.run_angle(200, 120, then=Stop.BRAKE, wait=True) #זרוע זזה לגובה של המסיל
+robot.straight(445) #מתקדמת לרכבת
+
+
+robot.turn(90) #פונה
+robot.straight(40) #מתקדמת למסיל 
+#robot.turn(-30) #פונה ומורידה את המסיל
+
+#robot.straight(300) #הולכת למסוק
+#robot.turn(40) #פונה כדי שתהיה עם גב למסוק
+#robot.straight(-10) #מתנגשת במסוק
+#robot.straight(10) #יוצאת מהמסוק
+
+#robot.turn(-40) #פונה לכיוון הרכבת
+#tiny_motor.run_angle(200, -170, then=Stop.BRAKE, wait=True) #זרוע זזה כדי שתוכל לדחוף את הרכבת
+#robot.straight(600) #דוחפת את הרכבת
+
+#robot.straight(-500) #הולכת אחורה לכיוון המנוף
+#robot.turn(90) #פונה לכיוון הבית
+#robot.straight(600) #מתקדמת לכיוון הבית
+#robot.turn(-30) #פונה לכיוון הבית
+#robot.straight(100) #נוסעת הביתה
